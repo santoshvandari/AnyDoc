73cd64c72f434aa1422c7986e0ff96adcb5e2d42
Santosh Bhandari <santoshvandari1@gmail.com>
2024-11-04 22:30:10 +0545

    Updated

--- lib/filehandler/doc_handler.dart
+++ lib/filehandler/doc_handler.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 
 class DocViewer extends StatelessWidget {
-  final 
   const DocViewer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
   }
- 
 }
