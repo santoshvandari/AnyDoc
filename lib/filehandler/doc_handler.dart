8a3c71715a9c320e0d0c0631632ab1ed981ae6cc
Santosh Bhandari <santoshvandari1@gmail.com>
2024-11-10 09:32:46 +0545

    Added the Document reader too

--- lib/filehandler/doc_handler.dart
+++ lib/filehandler/doc_handler.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:anydoc/screens/home_screen.dart';
 import 'package:docx_to_text/docx_to_text.dart';
 import 'package:flutter/material.dart';
 
@@ -12,46 +13,79 @@ class DocxViewer extends StatefulWidget {
 }
 
 class _DocxViewerState extends State<DocxViewer> {
+  String content = "";
+  bool isLoading = true;
+
   @override
   void initState() {
-    readFromDocxFile();
     super.initState();
+    readFromDocxFile();
   }
 
-  String content = "";
-  Future<String> readFromDocxFile() async {
-    final file = File(widget.filePath);
-    final bytes = await file.readAsBytes();
-    content = docxToText(bytes);
-    setState(() {});
-    return content; // docx to text
+  Future<void> readFromDocxFile() async {
+    try {
+      final file = File(widget.filePath);
+      final bytes = await file.readAsBytes();
+      content = docxToText(bytes);
+    } catch (e) {
+      _showUnsupportedFileTypeDialog(context);
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  void _showUnsupportedFileTypeDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Unsupported File Type'),
+          actions: [
+            Center(
+              child: TextButton(
+                onPressed: () {
+                  Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(
+                        builder: (context) => const HomeScreen(),
+                      ),
+                      (route) => false);
+                },
+                child: const Text('OK'),
+              ),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('DOCX Viewer'),
+        title: const Text('AnyDoc'),
         backgroundColor: const Color(0xFF3E699C),
         foregroundColor: Colors.white,
         centerTitle: true,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Stack(
-          children: [
-            Container(
-              color: Colors.white,
-              width: MediaQuery.sizeOf(context).width,
-              child: SingleChildScrollView(
-                child: Text(
-                  content,
-                  style: const TextStyle(fontSize: 16, color: Colors.black),
+        padding: const EdgeInsets.all(8.0),
+        child: isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : SingleChildScrollView(
+                child: Container(
+                  color: Colors.white,
+                  width: MediaQuery.sizeOf(context).width,
+                  child: Text(
+                    content,
+                    style: const TextStyle(fontSize: 16, color: Colors.black),
+                  ),
                 ),
               ),
-            )
-          ],
-        ),
       ),
     );
   }
